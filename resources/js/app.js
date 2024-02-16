e29a1767ddb59704634ddd4570f0994c2e29c879
zoe_tsai0418 <zoe@zhezhe.com.tw>
2024-02-16 17:19:10 +0800

    route調整

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import Nav from './components/navComponent';
 //pages
 import Home from './pages/Home';
 import About from './pages/About';
-// import NotFound from './pages/NotFound';
+import NotFound from './pages/NotFound';
 
 import { Routes, Route } from 'react-router-dom';
 
@@ -14,9 +14,10 @@ const App = () => {
   return (
     <>
         <Nav />
-        <Routes >
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="*" element={<NotFound />} /> {/* 默认路由 */}
         </Routes>
     </>
   );
